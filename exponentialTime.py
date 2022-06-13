f0c54c3562f53d3ebc7034c5b519c27dc09208f1
sophiewanjiku <98793987+sophiewanjiku@users.noreply.github.com>
2022-06-13 05:36:52 +0300

    "big O notation types, besection method and sorting algorithims"

--- exponentialTime.py
+++ exponentialTime.py
@@ -1,13 +1,14 @@
 import matplotlib.pyplot as plt
 import numpy as np
+#for y=2^x
 
-x = [2, 4, 6, 8, 10, 12]
-y = [2, 2, 2, 2, 2, 2]
+x = [1, 2, 3, 4, 5]
+y = [2, 4, 8, 16, 32]
 
 
 plt.plot(x, y, 'b')
 plt.xlabel('inputs')
 plt.ylabel('steps')
-plt.title('Constant Complexity')
+plt.title('exponential Complexity')
 plt.show()
 
